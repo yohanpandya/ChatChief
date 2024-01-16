59eefb2eb5199699bafeff87a2c4d2f1ced3ea69
Yohan Pandya <yvpandya@wisc.edu>
2024-01-15 16:44:14 -0800

    added cors in backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors'); // Add this line to import the cors middleware
 const app = express();
-const profile = require('./profile')
+const profile = require('./profile');
 
-app.use('/profile',profile)
+// Use CORS middleware without specifying allowed origins (allows all origins)
+app.use(cors());
 
+app.use('/profile', profile);
 
-const port = process.env.PORT||8000;
-app.listen(port,()=>{
-    console.log(`Server is running at ${port}`)
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
 });
